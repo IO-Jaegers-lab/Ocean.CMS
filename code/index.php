967ae97c8d6af8f49bd212973ab1ebcb84008b98
Kent vejrup Madsen <kent.vejrup.madsen@designermadsen.dk>
2023-01-20 18:31:33 +0100

    setup of index

--- code/index.php
+++ code/index.php
@@ -1,4 +1,12 @@
 <?php
+    require_once 'vendor/autoload.php';
+
+    use \IoJaegers\OceanCms\OceanCMS;
 
 
+    $engine = new OceanCMS();
+
+    $engine->initialise();
+    $engine->execute();
+    $engine->clean();
 ?>
---
+++ code/src/OceanCMS.php
@@ -0,0 +1,24 @@
+<?php
+	namespace IoJaegers\OceanCms;
+
+
+	class OceanCMS
+	{
+        function initialise(): void
+        {
+
+        }
+
+
+        function execute(): void
+        {
+
+        }
+
+
+        function clean(): void
+        {
+
+        }
+	}
+?>
